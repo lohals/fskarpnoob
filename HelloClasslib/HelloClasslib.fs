838a0038912285bfa43641e85bd8f68db86b3150
Rasmus Lohals <rll@schultz.dk>
2018-06-14 00:50:31 +0200

    added code

--- HelloClasslib/HelloClasslib.fs
+++ HelloClasslib/HelloClasslib.fs
@@ -1,4 +1,14 @@
 namespace HelloClasslib
 
-type HelloClasslib() = 
-    member this.X = "F#"
+module PigLatin =
+    let toPigLatin (word: string) =
+        let isVowel (c: char) =
+            match c with
+            | 'a' | 'e' | 'i' |'o' |'u'
+            | 'A' | 'E' | 'I' | 'O' | 'U' -> true
+            |_ -> false
+        
+        if isVowel word.[0] then
+            word + "yay"
+        else
+            word.[1..] + string(word.[0]) + "ay"
